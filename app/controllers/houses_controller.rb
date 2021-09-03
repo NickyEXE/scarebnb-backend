2b0f0c49369cbe5b248edabc789d44c448226a1d
NickyEXE <dovern42@gmail.com>
2021-09-03 14:35:35 -0400

    create for house

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -5,4 +5,15 @@ class HousesController < ApplicationController
     render json: houses
   end
 
+  def create
+    house = House.create(house_params)
+    render json: house
+  end
+
+  private
+
+  def house_params
+    params.require(:house).permit(:name, :address, :city, :state, :haunting, :image_url)
+  end
+
 end
