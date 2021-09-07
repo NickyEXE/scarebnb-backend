5eac72cc957f3e997e814638003135da87f0fec3
NickyEXE <dovern42@gmail.com>
2021-09-07 19:14:39 -0400

    add user model

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,3 +1,4 @@
 class House < ApplicationRecord
   has_many :apartments
+  belongs_to :user
 end
--- app/serializers/house_serializer.rb
+++ app/serializers/house_serializer.rb
@@ -1,4 +1,9 @@
 class HouseSerializer < ActiveModel::Serializer
-  attributes :id, :name, :address, :city, :state, :haunting, :image_url
+  attributes :id, :name, :address, :city, :state, :haunting, :image_url, :username
   has_many :apartments
+
+  def username
+    self.object.user.username
+  end
+
 end
